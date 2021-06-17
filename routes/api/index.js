f7bcc624e9633336055993711e384cb2ed1eb6fa
Nick Reisner <nreisne@wgu.edu>
2021-06-16 20:57:37 -0500

    adding index.js base

---
+++ routes/api/index.js
@@ -0,0 +1,30 @@
+// import seed files
+const seedUsername = require('./username-seeds');
+const seedPassword = require('./password-seeds');
+const seedEmail = require('./email-seeds');
+
+// import database connection from connection.js
+const sequelize = require('../config/connection');
+
+// seed all data 
+const seedAll = async () => {
+  
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  
+  await seedCategories();
+  console.log('\n----- CATEGORIES SEEDED -----\n');
+
+  await seedProducts();
+  console.log('\n----- PRODUCTS SEEDED -----\n');
+
+  await seedTags();
+  console.log('\n----- TAGS SEEDED -----\n');
+
+  await seedProductTags();
+  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
