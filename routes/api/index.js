15044b8671fb5d6c58ade53baad607aed7d9f261
Nick Reisner <nreisne@wgu.edu>
2021-06-16 21:05:46 -0500

    adding index.js route code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,27 +1,12 @@
-// import seed files
-const seedUsername = require('./username-seeds');
-const seedPassword = require('./password-seeds');
-const seedEmail = require('./email-seeds');
+// import routes
+const router = require('express').Router();
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
 
-// import database connection from connection.js
-const sequelize = require('../config/connection');
+// prefix all routes 
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 
-// seed all data 
-const seedAll = async () => {
-  
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  
-  await seedCategories();
-  console.log('\n----- USERNAME SEEDED -----\n');
-
-  await seedProducts();
-  console.log('\n----- PASSWORD SEEDED -----\n');
-
-  await seedTags();
-  console.log('\n----- EMAIL SEEDED -----\n')
-
-  process.exit(0);
-};
-
-seedAll();
+module.exports = router;
--- routes/api/username-route.js
+++ routes/api/username-route.js
@@ -1,33 +1,13 @@
-// import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+// import routes
+const router = require('express').Router();
+const apiRoutes = require('./api');
 
-// import database connection from connection.js
-const sequelize = require('../config/connection.js');
+// prefix routes
+router.use('/api', apiRoutes);
 
-// initialize username model (table) by extending off Sequelize's Model class
-class username extends Model {}
+// send error message for wrong routes
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>")
+});
 
-// set up fields and rules for username model
-username.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    username_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'username'
-  }
-);
-
-module.exports = username;
+module.exports = router;
