0748db173db0a3319d8aa6b6eb29f47c4561296a
Davsav <davidbyu6@gmail.com>
2021-06-21 11:34:25 -0600

    added a user model

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('todo_app', 'root', '2021$@uofu#byu9D', {
+  : new Sequelize('music_app', 'root', '2021$@uofu#byu9D', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql',
