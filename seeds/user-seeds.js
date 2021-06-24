8cc7ae4b45086617779b2f6145fc4b773f0d4de5
Nick Reisner <nreisne@wgu.edu>
2021-06-23 20:34:39 -0500

    fixing seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,21 +4,37 @@ const { User } = require('../models');
 // array of data for User class
 const userData = [
   {
-    username: 'Musicman123',
-    password: 'Password1'
+    firstName: 'Natalie',
+    lastName: 'Smith',
+    email: 'natalie.smith12345@gmail.com',
+    password: 'Password1',
+    socialSite: 'Facebook', 
+    bio: 'I like Rock music and concerts hit me up if you want to rock!',
+},
+  {
+    firstName: 'Sarah',
+    lastName: 'Jimenez',
+    email: 'sarah.jimenez12345@gmail.com',
+    password: 'Password2',
+    socialSite: 'Github', 
+    bio: 'I like to pop and lock hit me up if to jam out and dance to your favorite jam!',
   },
   {
-    username: 'MusicKing95',
-    password: 'Password2'
+    firstName: 'Kate',
+    lastName: 'Johnson',
+    email: 'kate.johnson12345@gmail.com',
+    password: 'Password3',
+    socialSite: 'Facebook', 
+    bio: 'I like pop music and top 40 hit me up to talk about the most current songs!',
   },
   {
-    username: 'ImagineDragonsLover',
-    password: 'Password3'
+    firstName: 'David',
+    lastName: 'Carillo',
+    email: 'david.carillo12345@gmail.com',
+    password: 'Password4',
+    socialSite: 'Github', 
+    bio: 'I like latin and EDM hits hit me up if you like Enrique Iglesias!',
   },
-  {
-    username: 'Songsaremyjams33',
-    password: 'Password4'
-  }
 ];
 
 // create and insert multiple Users using userData
