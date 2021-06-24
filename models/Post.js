bb877c4ffef82640a564d4185b837b372a158005
Davsav <davidbyu6@gmail.com>
2021-06-23 21:30:36 -0600

    created backend for voting

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Post extends Model {
+    static upvote(body, models) {
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'artist',
+                    'title',
+                    'genre',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                      ]
+                ]
+            });
+        });
+    }
+}
 
 Post.init(
     {
@@ -15,12 +39,9 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false
         },
-        musicVid_url: {
+        title: {
             type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isUrl: true
-            }
+            allowNull: true
         },
         genre: {
             type: DataTypes.STRING,
