affcd435c7af750a0e701eaaf12a1b89dd36b4c7
Saira <sairaenriquez@Sairas-MacBook-Pro.local>
2021-06-28 12:20:39 -0600

    issue 2-auth

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const passport = require('passport');
+
 
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
@@ -47,3 +49,38 @@ sequelize.sync({ force: true }).then(() => {
     console.log('App listening on PORT ' + PORT);
   });
 });
+
+///loging-off 
+
+router.get('login',(req,res) => {
+  //passport 
+  res.send('login'); 
+})
+
+router.get('logout',(req,res) => {
+  //passport 
+  res.send('logout'); 
+})
+
+//Github
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+//Google 
+app.get('/auth/google',
+  passport.authenticate('google', { 
+    scope: ['profile'] }));
+
+route.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
