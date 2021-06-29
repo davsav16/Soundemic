8df3b7928135793eb3c6944118be08a2493215ae
katiebbugs <79028196+katiebbugs@users.noreply.github.com>
2021-06-28 18:32:20 -0600

    testing fixes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const hbs = exphbs.create({ helpers });
 
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
